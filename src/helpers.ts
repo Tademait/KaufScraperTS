081afa2897f55b7b8cc3c9f1112c943be0da597d
Tademait <Tademait@github.com>
2023-09-22 22:16:30 +0200

    edit the date formatter

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,14 +2,20 @@ type FunctionToWrap = (...args: any[]) => any
 
 export function logWrapper(originalFunc: FunctionToWrap) {
     return async function(...args: any[]) {
-      console.log(`[${formatDate(Date.now())}]: Scraper started...`);
+      console.log(`[${formatDate(new Date)}]: Scraper started...`);
       const result = await originalFunc.apply(args);
-      console.log(`[${formatDate(Date.now())}]: Scraper finished.`);
+      console.log(`[${formatDate(new Date)}]: Scraper finished.`);
       return result;
     };
   }
 
-export function formatDate(date: number) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-  return new Date(date).toLocaleDateString('en-UK');
+export function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}/${month}/${day} | ${hours}:${minutes}:${seconds}`;
 }
